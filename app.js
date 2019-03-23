c8a3dc0eb5a5db064b46e0ad869371d547adb2a5
June Hyok Cho <jcho0627@gmail.com>
2019-03-23 18:35:10 +0100

    selector exercise

---
+++ app.js
@@ -0,0 +1,5 @@
+const firstP = document.getElementById("first");
+const secondP = document.getElementsByClassName("special")[0];
+const thirdP = document.querySelector("#first");
+const fourthP = document.getElementsByTagName("p")[0];
+const fifthP = document.querySelector("h1 + p");
